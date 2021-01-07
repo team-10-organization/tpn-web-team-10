2ae16cb495063e17ca1a0fed67b8a43cfe7404db
heyhadi <hadi.munawirul@gmail.com>
2021-01-08 01:19:41 +0700

    add google login controller

--- server/controller/users.js
+++ server/controller/users.js
@@ -2,6 +2,42 @@
 
 class User {
 
+    static async googleLogin(req, res) {
+        try {
+        const google_token = req.body.google_token;
+        const client = new OAuth2Client(process.env.CLIENT_ID);
+        const ticket = await client.verifyIdToken({
+            idToken: google_token,
+            audience: process.env.CLIENT_ID,
+        });
+        const payload = ticket.getPayload();
+        console.log("ini payload");
+        console.log(payload);
+        const email = payload.email;
+        const user = await User.findOne({ where: { email: payload.email } });
+        if (user) {
+            const accessToken = loginToken({
+            id: user.id,
+            email: user.email,
+            });
+            res.status(200).json({accessToken});
+        } else {
+            const userObj = { 
+            email,
+            password: "random",
+            };
+            const newUser = await User.create(userObj);
+            const accessToken = loginToken({
+            id: newUser.id,
+            email: newUser.email,
+            });
+            res.status(200).json({accessToken});
+        }
+        } catch (err) {
+            console.log(err);
+        }
+    }
+
 }
 
 module.exports = User
