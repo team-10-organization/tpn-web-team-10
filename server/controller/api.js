5a01cca9de7cd5b49c53a533471105c4c4b0e64b
Miftakhunnurudin <Miftakhunnurudin@gmail.com>
2021-01-07 23:06:52 +0700

    add get weather data from public api weather

--- server/controller/api.js
+++ server/controller/api.js
@@ -1,7 +1,22 @@
 // API
+const axios = require('axios')
 
 class Api {
-
+    static getWeatherbyCoords(req,res,next){
+        let {lat,lon} = req.query
+        const url = 'https://api.openweathermap.org/data/2.5/weather'
+        axios.get(url,{
+            params:{
+                lat,
+                lon,
+                appid:'bea7899e82e2e085b14050d260ac7a49'
+            }
+        })
+        .then(response=>{
+            res.status(200).json(response.data)
+        })
+        .catch(next)
+    }
 }
 
 module.exports = Api
