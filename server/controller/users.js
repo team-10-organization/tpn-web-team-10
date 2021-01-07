00c4c8efb9352c62cae246f7a2a5ed2cfcb49b70
Edwin Dharmawan <edwindharmawan@gmail.com>
2021-01-08 00:14:40 +0700

    add register & login

--- server/controller/users.js
+++ server/controller/users.js
@@ -1,7 +1,57 @@
-// login register
+const { User } = require('../models/')
+const { comparePassword } = require('../helper/bcrypt')
+const { generateToken } = require('../helper/jwt')
 
-class User {
-
+class UserController {
+    static async register (req, res) {
+        const { userName, email, password } = req.body
+        try {
+            const data = await User.create({
+                userName,
+                email,
+                password
+            })
+            res.status(201).json({
+                userName,
+                email,
+            })
+        } catch (err) {
+            res.status(401).json(err)
+        }
+    }
+    static async login (req, res) {
+        try {
+            const { email, password } = req.body
+            const user = await User.findOne({
+                where: { email }
+            })
+            if (!user) {
+                res.status(401).json({
+                    msg: 'Email or password not defined'
+                })
+            } else {
+                const match = comparePassword(password, user.password)
+                if (match) {
+                    //jwt
+                    const userDATA = {
+                        id: user.id,
+                        userName: user.userName,
+                        email: user.email
+                    }
+                    const access_token = generateToken(userDATA)
+                    res.status(200).json({
+                        access_token: access_token
+                    })
+                } else {
+                    res.status(401).json({
+                        msg: 'Email or password not defined'
+                    })
+                }
+            }
+        } catch (err) {
+            res.status(401).json(err)
+        }
+    }
 }
 
-module.exports = User
+module.exports = UserController
